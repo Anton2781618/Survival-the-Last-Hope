712b96c5c88c194a895b9eb0646f91ca27bdd49b
Anton2781618 <69422633+Anton2781618@users.noreply.github.com>
2024-09-12 23:06:09 +0300

    Вернул чанк менеджер

---
+++ Assets/MyScripts/Chank System (definiton base)/Chank.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chank : MonoBehaviour
+{
+    public Terrain Terrain;
+    public Vector2Int ChankPosition;
+
+    public void Init()
+    {
+        Terrain = transform.GetComponent<Terrain>();
+
+        ChankPosition = new Vector2Int((int)(transform.position.x / Terrain.terrainData.size.x), (int)(transform.position.z / Terrain.terrainData.size.x));        
+        
+    }
+}
